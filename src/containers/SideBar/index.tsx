c57f2cb3308f1fc58324b830bbd1a6e53ce6fad3
Carvs10 <joaovcl10@hotmail.com>
2023-11-23 16:59:47 -0300

    add filter props

--- src/containers/SideBar/index.tsx
+++ src/containers/SideBar/index.tsx
@@ -1,3 +1,20 @@
-const SideBar = () => <aside>Sidebar</aside>
+import CardFilter from '../../components/CardFilter'
+import * as S from './styles'
+
+const SideBar = () => (
+  <S.Aside>
+    <div>
+      <S.Field type="text" placeholder="Buscar" />
+      <S.Filters>
+        <CardFilter subtitle="pendentes" counter={1} />
+        <CardFilter subtitle="concluídas" counter={2} />
+        <CardFilter subtitle="urgentes" counter={3} />
+        <CardFilter subtitle="importantes" counter={4} />
+        <CardFilter subtitle="normal" counter={5} />
+        <CardFilter subtitle="todas" counter={10} active />
+      </S.Filters>
+    </div>
+  </S.Aside>
+)
 
 export default SideBar
